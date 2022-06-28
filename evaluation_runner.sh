f21864ce79d247f2f1d60cb1fac90ad829ddfd4f
ak <akhakhar@allegro.seas.upenn.edu>
2022-06-28 01:35:07 -0400

    eval runner bug fix

--- evaluation_runner.sh
+++ evaluation_runner.sh
@@ -1,13 +1,13 @@
-for num_predictions in 1 2 3 4 5 6 7 8 9 10
+for num_predictions in 1 2 3 4 5 6 7 8 9 10 20 50 100
 do
     echo num_predictions $num_predictions
     python3 code-prediction-set/config_update.py $((num_predictions+4)) $num_predictions
-    for startindx in {0..1485..15}
+    for startindx in {0..200..10}
     do
         make serve --ignore-errors >> log &
         echo startindx $startindx
         sleep 30
-        python3 code-prediction-set/runner.py $num_predictions --startindx $startindx --endindx $((startindx +15))
+        python3 code-prediction-set/runner.py $((num_predictions+4)) $num_predictions --startindx $startindx --endindx $((startindx +10))
         podman kill `podman ps -q`
     done
 done
