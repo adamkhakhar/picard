e982760859fab6b0e84f1f65e14d61aaa9adf8ec
ak <akhakhar@allegro.seas.upenn.edu>
2022-08-31 13:00:32 -0400

    rm unused var

--- code-prediction-set/sql_tree/tokenize_spider.py
+++ code-prediction-set/sql_tree/tokenize_spider.py
@@ -5,11 +5,9 @@ def tokenize(sql_tokens: List[str]) -> List[str]:
     print("sql_tokens", sql_tokens)
     clean_toks = []
     # remove all tokens which are before select token
-    select_included = False
     for i in range(len(sql_tokens)):
         curr_tok = sql_tokens[i]
         if curr_tok.lower() == "select":
-            select_included = True
             sql_tokens = sql_tokens[i:]
             break
     # remove white space tokens
