34ec528782c3515b87c1d0e5a886a750747eaa3a
ak <akhakhar@allegro.seas.upenn.edu>
2022-09-11 15:21:35 -0400

    optimze tree via z3 theorem prover

---
+++ code-prediction-set/sql_tree/optimize_sql_tree.py
@@ -0,0 +1,54 @@
+from z3 import *
+import numpy as np
+
+
+def add_tree_constraints(s, tree):
+    ordered_probabilities = []
+    indicator_variables = []
+    map_node_to_indicator = {}
+    # traverse tree and create ordered names of nodes and probabilities corresponding to that node
+    # create boolean variables for inclusion in tree
+    node_number = 0
+    q = [tree]
+    while len(q) > 0:
+        curr_node = q.popleft()
+        curr_indicator = Bool(curr_node.name + ":" + str(node_number))
+        indicator_variables.append(curr_indicator)
+        map_node_to_indicator[curr_node] = curr_indicator
+        ordered_probabilities.append(curr_node.prob)
+        for c in curr_node.children:
+            q.append(c)
+        node_number += 1
+
+    # add constraints (linear time)
+    q = [tree]
+    while len(q) > 0:
+        curr_node = q.popleft()
+        curr_indicator_variable = map_node_to_indicator[curr_node]
+        for c in curr_node.children:
+            child_indicator_variable = map_node_to_indicator[c]
+            s.add(Implies(child_indicator_variable, curr_indicator_variable))
+            q.append(c)
+
+    assert len(ordered_probabilities) == len(indicator_variables)
+    return ordered_probabilities, indicator_variables
+
+
+def solve_optimization(tree, max_cost_threshold):
+    """
+    solves optimization problem defined in https://www.overleaf.com/project/6304f33ff542595b403d373e
+    """
+    s = Solver()
+    # add node removal constraints
+    probabilities, indicator_variables = add_tree_constraints(s, tree)
+    # add threshold constraint
+    s.add(sum([-1 * variables[i] * indicator_variables[i] for i in range(len(variables))]) <= max_cost_threshold)
+    # add optimization
+    o = Optimize()
+    o.maximize(sum([-1 * variables[i] * indicator_variables[i] for i in range(len(variables))]))
+
+
+if __name__ == "__main__":
+    x = Int("x")
+    y = Int("y")
+    solve(x > 2, y < 10, x + 2 * y == 7)
